5361ba189fad83b8d70e96748963a3e14ceab3e0
ArnoldYing <arnoldying825@gmail.com>
2020-10-17 16:20:44 -0700

    updated movement physics

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,45 +2,38 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public bool jumpable = false;
     public Rigidbody rb;
-    // public float forwardForce = 2000f;
-    public float moveForce = 2000f;
+    public float speed = 20f;
+    public float jumpHeight = 200f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        // float y = Input.GetAxis("Jump") * speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
-        // forward
-        if (Input.GetKey("w"))
-        {
-            rb.AddForce(0, 0, moveForce * Time.deltaTime);
-        }
-        
-        // backward
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce(0, 0, -moveForce * Time.deltaTime);
-        }
+        transform.position += new Vector3(x, 0, z);
 
-        // right
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(moveForce * Time.deltaTime, 0, 0);
-        }
-        
-        // left
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-moveForce * Time.deltaTime, 0, 0);
-        }
-
-        // up
+        // jump
         if (Input.GetKey("space"))
         {
-            rb.AddForce(0, moveForce * Time.deltaTime, 0);
+            if (jumpable) 
+            {
+                jumpable = false;
+                // Debug.Log("Jump: " + transform.position.y);
+                rb.AddForce(0, jumpHeight, 0);
+            }
         }
+    }
 
+    void OnCollisionEnter (Collision collisionInfo) 
+    {
+        // Debug.Log(collisionInfo.collider.name);
+        if (collisionInfo.collider.tag == "Terrain") 
+        {
+            jumpable = true;
+        }
     }
 }
