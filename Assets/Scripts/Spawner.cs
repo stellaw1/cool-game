a3ce158c87edda87c3e81db26b0c07ca010d0e75
Seann Wang <63817650+wangseann@users.noreply.github.com>
2020-10-18 01:06:01 -0700

    Add files via upload

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 10)
+        {
+            Instantiate(theEnemy, new Vector3(xPos, 342, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+}
