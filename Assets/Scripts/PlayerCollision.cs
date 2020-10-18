95ee3cf292d91102dac1aa1b52a3ff99ba2c5844
ArnoldYing <arnoldying825@gmail.com>
2020-10-17 18:07:51 -0700

    added gun

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter (Collision collisionInfo) 
+    {
+        // Debug.Log(collisionInfo.collider.name);
+        if (collisionInfo.collider.tag == "Obstacle") 
+        {
+            Debug.Log("we've hit an obstacle");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -20f;
+    public float jumpHeight = 3f;
+    
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ground check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+        // horizontal movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // vertical movement
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+
+        // moving
+        Vector3 move = transform.right * x + velocity / speed + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
